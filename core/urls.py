178c0e62103e0fc87317b9c8d75e29ad12d48256
Akobir <akobirortiqov40@gmail.com>
2025-07-23 16:47:01 +0500

    fix header and footer

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_page_view
+from pages.views import home_page_view, contact_page_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page_view),
+    path('contact/', contact_page_view),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_page_view(request):
     return render(request, template_name='index.html')
+
+def contact_page_view(request):
+    return render(request, template_name='htmls/contact.html')
