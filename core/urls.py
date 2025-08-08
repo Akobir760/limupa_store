42e6646a2462530fb0c8059c9437abc30bf0f933
Akobir <akobirortiqov40@gmail.com>
2025-08-09 00:11:52 +0500

    fix translate

--- core/urls.py
+++ core/urls.py
@@ -18,13 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 
-urlpatterns = [
+urlpatterns = []
+
+urlpatterns += i18n_patterns (
     path('admin/', admin.site.urls),
     path('', include('pages.urls', namespace='pages')),
     path('user', include('user.urls', namespace='user')),
-]
+)
 
 
 if settings.DEBUG:
